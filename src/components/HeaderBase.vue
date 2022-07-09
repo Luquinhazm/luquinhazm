ab934fbcdfa2d25ee8a242c55e70b358fe8d0179
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-09 19:52:50 -0300

    Header

--- src/components/HeaderBase.vue
+++ src/components/HeaderBase.vue
@@ -1,6 +1,15 @@
 <template>
-    <header>
-        <p class="text-4xl text-yellow-400">Ola Mundo</p>
+    <header class="bg-slate-200 h-20 flex items-center justify-between px-6">
+        <span class="text-xl uppercase font-bold text-gray-800">Luquinhazm</span>
+        <nav class="text-gray-900 text-lg uppercase flex gap-6 ">
+            <ul class="flex items-center gap-4">
+                <li><a href="">Skills</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Contatos</a></li>
+            </ul>
+            <button class="bg-gray-800 text-slate-100 px-2 py-1 font-semibold border-none ">Download CV</button>
+        </nav>
     </header>
 </template>
 <script>
