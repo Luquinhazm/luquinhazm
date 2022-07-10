b90c83560e9cc10cae307e0095857a93d5ac1a8f
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-10 14:08:51 -0300

    adicionando links

--- src/components/FooterBase.vue
+++ src/components/FooterBase.vue
@@ -2,9 +2,9 @@
     <section class="min-h-[20vh] bg-black text-slate-50 flex flex-col items-center justify-center gap-4">
         <a href="mailto:luquinhazm@gmail.com">luquinhazm@gmail.com</a>
         <div class="flex gap-4 text-3xl">
-            <a href=""><i class="fa-brands fa-github"></i></a>
-            <a href=""><i class="fa-brands fa-twitter"></i></a>
-            <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
+            <a href="https://github.com/Luquinhazm" target="_blank"><i class="fa-brands fa-github hover:text-slate-300 transition"></i></a>
+            <a href="https://twitter.com/luquinhazm" target="_blank"><i class="fa-brands fa-twitter hover:text-slate-300 transition"></i></a>
+            <a href="https://www.linkedin.com/in/luquinhazm/" target="_blank"><i class="fa-brands fa-linkedin-in hover:text-slate-300 transition"></i></a>
         </div>
     </section>
 </template>
