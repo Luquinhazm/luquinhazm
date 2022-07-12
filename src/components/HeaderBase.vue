54ab4a7f647820ee70a01a582cefb9a307fbd8a6
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-12 15:29:52 -0300

    Responsividade Header

--- src/components/HeaderBase.vue
+++ src/components/HeaderBase.vue
@@ -1,8 +1,8 @@
 <template>
 	<header class="h-16 flex items-center justify-between px-10">
 		<a href="" class="text-xl uppercase font-bold text-gray-800">Luquinhazm</a>
-		<nav class="  flex gap-6 ">
-			<ul class="flex items-center gap-8">
+		<nav class="flex gap-6 ">
+			<ul class="sm:hidden flex items-center gap-8">
 				<li><a class="hover:text-gray-500" href="#skills">Habilidades</a></li>
 				<li><a class="hover:text-gray-500" href="#projetos">Projetos</a></li>
 				<li><a class="hover:text-gray-500" href="#contato">Contato</a></li>
