4eef679554a1410a002c74cc56d91bb94b46d1bf
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-12 16:03:17 -0300

    Main Responsividade

--- src/components/HeroBase.vue
+++ src/components/HeroBase.vue
@@ -1,32 +1,32 @@
 <template>
-	<main class="flex mt-16 justify-around min-h-[78vh] ">
-		<div class="w-2/4 flex flex-col justify-around">
-			<div class="max-w-sm">
-				<h1 class="font-bold text-2xl uppercase mb-2">Lorem, ipsum.</h1>
+	<main class="flex mt-16 px-4 justify-around min-h-[78vh] md:flex-wrap-reverse md:justify-start  lg:mt-4  ">
+		<div class=" flex px-4 flex-col gap-4 justify-around">
+			<div class="max-w-[450px] w-[95%]">
+				<h1 class="font-bold text-2xl uppercase mt-6 mb-2">Lorem, ipsum.</h1>
 				<p class="mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt officia magnam ad ea unde.
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, tempora.</p>
-				<div class="flex gap-8">
-					<button
-						class="bg-gray-900 text-white px-2 py-1 font-semibold border-none hover:scale-105 hover:bg-gray-300 hover:text-black transition-all delay-75 ease-in-out   ">
-						Download CV
-					</button>
-					<button
-						class="bg-gray-900 text-white px-2 py-1 font-semibold border-none hover:scale-105 hover:bg-gray-300 hover:text-black transition-all delay-75 ease-in-out">Entre
-						em Contato</button>
+				
+				<div class="flex gap-8 sm:flex-wrap sm:gap-2 ">
+					<BtnPrimary>Download CV</BtnPrimary>
+					<BtnPrimary>Entre em Contato</BtnPrimary>
 				</div>
 			</div>
-			<h1 class="text-3xl font-bold">Olá! Me Chamo Lucas Mendes e sou um Desenvolvedor Front-End</h1>
+			
+			<h1 class="text-3xl max-w-[720px] font-bold sm:hidden">Olá! Me Chamo Lucas Mendes e sou um Desenvolvedor Front-End</h1>
+			
 		</div>
 
-		<div class="w-2/3 max-w-[472px]">
-			<img class="max-w-[472px] rounded-md shadow-xl shadow-gray-400" src="../assets/Perfil.jpg"
+		<div class="lg:w-[60%] md:h-full md:px-4 max-w-[472px]">
+			<img class="max-w-[472px] w-[100%] rounded-md shadow-xl shadow-gray-400  " src="../assets/Perfil.jpg"
 				alt="Foto de perfil Lucas">
 		</div>
 
 	</main>
 </template>
 <script>
+import BtnPrimary from './Buttons/BtnPrimary.vue';
 export default {
-	name: 'HeroBase'
+    name: "HeroBase",
+    components: { BtnPrimary }
 }
 </script>
