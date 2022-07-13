425f31954dc19d48a4b7aef2db9abb1665c743de
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-13 14:04:44 -0300

    v-for projetos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-	<nav>
+	
 		<HomeView />
-	</nav>
+	
 </template>
 
 <script>
--- src/components/ProjetosSecao.vue
+++ src/components/ProjetosSecao.vue
@@ -4,70 +4,91 @@
 		<p class="text-center">Alguns dos meus projetos já feitos</p>
 
 		<div class="mb-8 mt-12">
-
+			
 			<div class="mt-6 mb-20 mx-auto flex flex-col justify-center items-center w-11/12">
 				<h3 class="text-2xl font-bold uppercase tracking-wider mb-6">Freelancers</h3>
-
 				<div class="flex gap-4">
 					<div class="flex flex-wrap gap-8">
-
-						<div class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150 w-98">
-							<a target="_blank" href="http://www.shstecnologia.com/"> 
-								<img src="../assets/shstech.png " class="h-64 mb-2 w-full max-w-sm flex-shrink-0" alt=""> 
+						<div  
+						v-for=" projeto in projetos" :key="projeto.id"
+						class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150 w-98">
+							<a target="_blank" :href="projeto.site"> 
+								<img :src="projeto.img" class="h-64 mb-2 w-full max-w-sm flex-shrink-0" alt=""> 
 							</a>
-							<span class="text-lg px-2 text-gray-900">SHS Tecnologia</span>
+							<span class="text-lg px-2 text-gray-900">{{projeto.title}}</span>
 						</div>
-
-						<div class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150 w-98">
-							<a target="_blank" href="https://lutechinformatica.com.br/">
-								<img src="../assets/lutech.png " class="h-64 mb-2 w-full max-w-sm flex-shrink-0 " alt=""> 
-							</a>
-							<span class="text-lg px-2 text-gray-900">Lutech Informatica</span>
-						</div>
-
 					</div>
 				</div>
 			</div>
 
 			<div class="mt-14 mx-auto flex flex-col items-center">
 				<h3 class="text-2xl font-bold uppercase tracking-wider mb-6">Estudos</h3>
-
-
 				<div class="flex gap-4 justify-center max-w-[850px] ">
-
 					<div class="flex flex-wrap justify-center w-full gap-8">
-
-						<div class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150">
-							<a target="_blank" href="https://7days-of-code-nine.vercel.app/"> <img src="../assets/7daysofcode.png "
-									class="h-64 mb-2 w-full max-w-sm flex-shrink-0  " alt=""> </a>
-							<span class="text-lg px-2 text-gray-900">7 days of code | Alura</span>
-						</div>
-						
-						<div class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150">
-							<a target="_blank" href="https://pedra-papel-tesoura-xi-one.vercel.app/"> <img
-									src="../assets/pedrapapeltesoura.png " class="h-64 mb-2 w-full max-w-sm flex-shrink-0 " alt=""> </a>
-							<span class="text-lg  text-gray-900">Pedra, Papel, Tesoura <br> FrontEnd Mentor </span>
-						</div>
-						<div class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150">
-							<a target="_blank" href="https://ignitelabreact.vercel.app/"><img src="../assets/ignitelab.png "
-									class="h-64 mb-2 w-full max-w-sm flex-shrink-0  " alt=""> </a>
-							<span class="text-lg px-2 text-gray-900">IgniteLab | Rocketseat</span>
-						</div>
-						<div class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150">
-							<a target="_blank" href="https://advinhanumero.vercel.app/"><img src="../assets/advinhaNumero.png "
-									class="h-64 mb-2 w-full max-w-sm flex-shrink-0  " alt=""></a>
-							<span class="text-lg px-2 text-gray-900">Adivinha Número | Curso Em Video</span>
+						<div 
+						v-for=" estudo in estudos" :key="estudo.id"
+						class=" flex-shrink-0 text-center hover:scale-105 transition-transform ease-linear delay-150">
+							<a target="_blank" :href="estudo.site"> 
+								<img :src="estudo.img"
+								class="h-64 mb-2 w-full max-w-sm flex-shrink-0  " alt=""> </a>
+							<span class="text-lg px-2 text-gray-900">{{estudo.title}}</span>
 						</div>
 					</div>
 				</div>
 			</div>
+
 		</div>
 	</section>
 </template>
 
 <script>
-
 export default {
 	name: 'ProjetosSecao',
+data(){
+	return {
+		projetos:[
+			{
+					id: 0,
+					title: 'SHS Tecnologia',
+					img: require('../assets/shstech.png'),
+					site:'http://www.shstecnologia.com/'
+			},
+
+			
+			{
+				id: 1,
+				title: 'Lutech Informatica',
+				img: require('../assets/lutech.png'),
+				site:'https://lutechinformatica.com.br/'
+			},
+		],
+		estudos:[
+			{
+					id: 0,
+					title: '7 Days Of Code | Alura',
+					img: require('../assets/7daysofcode.png'),
+					site:'https://7days-of-code-nine.vercel.app/'
+			},
+			{
+					id: 1,
+					title: 'Pedra, Papel, Tesoura | FrontEnd Mentor',
+					img: require('../assets/pedrapapeltesoura.png'),
+					site:'https://pedra-papel-tesoura-xi-one.vercel.app/'
+			},
+			{
+					id: 2,
+					title: 'IgniteLab | Rocketseat',
+					img: require('../assets/ignitelab.png'),
+					site:'https://ignitelabreact.vercel.app/'
+			},
+			{
+					id: 3,
+					title: 'Advinha Número',
+					img: require('../assets/advinhaNumero.png'),
+					site:'https://advinhanumero.vercel.app/'
+			},
+		]
+	}
+}
 }
 </script>
