e8a9f85d51cdc9e51571ca8d637c24be30ff8826
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-15 21:54:57 -0300

    Projeto pagina

--- src/views/ProjetoView.vue
+++ src/views/ProjetoView.vue
@@ -1,10 +1,32 @@
 <template>
-  <section>
-    <h1 class="text-3xl font-bold"> Projetos</h1>
+  <section class="min-h-screen "> 
+    <h1 class="text-3xl text-center py-3 font-bold">Nome do Projetos</h1>
+    <div class="flex w-full min-h-[90vh] justify-around items-center">
+
+      <div class=" bg-black h-80">
+        <div class="w-[640px]">
+            <p class="text-white
+            ">jasfkahfuahfugsdfhasfjhv Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga provident sint cum quaerat rem nobis deserunt a voluptatem vel, est numquam nostrum illum reiciendis expedita consequuntur perferendis natus sit distinctio!</p>
+        </div>
+         
+      </div>
+
+      <div class="">
+        <div class="w-[500px] flex flex-col gap-3">
+          <h2 class="text-2xl font-semibold uppercase tracking-wide">Objetivos</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto in sed iure consequatur, animi enim earum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam earum quis iste omnis non repellat eos?</p>
+          <h2 class="text-2xl font-semibold uppercase tracking-wide">desafios</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae vitae libero quas corrupti consectetur ratione sit? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe eaque pariatur delectus nobis porro expedita culpa!</p>
+          <h3 class="text-xl font-semibold uppercase tracking-wide">ferramentas utilizadas</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis.</p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 <script>
+import HeaderBase from '@/components/HeaderBase.vue';
 export default {
-  
+  components:{ HeaderBase }
 }
 </script>
