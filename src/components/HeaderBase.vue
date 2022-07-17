7cfce4955b3a7645246acea1f74d907bda3ec049
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-17 00:46:24 -0300

    padding header

--- src/components/HeaderBase.vue
+++ src/components/HeaderBase.vue
@@ -1,5 +1,5 @@
 <template>
-	<header class="h-16 flex items-center justify-between px-10">
+	<header class="h-16 flex items-center justify-between px-10 md:px-5">
 		<a href="" class="text-xl uppercase font-bold text-gray-800">Luquinhazm</a>
 		<nav class="flex gap-6 ">
 			<ul class="sm:hidden flex items-center gap-8">
