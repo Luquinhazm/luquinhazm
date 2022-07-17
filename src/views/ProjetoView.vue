053058b14fc14de344c700ef7a9c17026127e79d
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-07-16 22:07:24 -0300

    Conteudo da Pagina de Projeto

--- src/views/ProjetoView.vue
+++ src/views/ProjetoView.vue
@@ -1,4 +1,4 @@
-<template>
+<template >
   <section v-for="projeto in projetos">
     <div v-if="projeto.id == $route.params.id">
       <div class="flex justify-center mt-4 items-center relative ">
@@ -30,9 +30,7 @@
             <h2 class="text-2xl font-semibold uppercase tracking-wide">Objetivos</h2>
             <p>{{ projeto.objetivo }}</p>
             <h2 v-if="projeto.desafio" class="text-2xl font-semibold uppercase tracking-wide">desafios</h2>
-            <p v-if="projeto.desafio">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae vitae libero quas
-              corrupti consectetur ratione sit? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe eaque
-              pariatur delectus nobis porro expedita culpa!</p>
+            <p v-if="projeto.desafio">{{projeto.desafio}}</p>
             <h3 class="text-xl font-semibold uppercase tracking-wide">ferramentas utilizadas</h3>
             <p>{{ projeto.ferramentas }} </p>
           </div>
@@ -64,29 +62,40 @@ export default {
           title: "Lutech Informatica",
           img: require("../assets/lutech.png"),
           site: "https://lutechinformatica.com.br/",
+          objetivo: 'Criação de um site institucional para a empresa ',
+          desafio:'Desenvolver o site completo com um curto prazo de entrega',
           ferramentas: 'HTML, CSS e FormSubmit para o envio de formulario de contato'
         },
         {
           id: 2,
           title: "7 Days Of Code | Alura",
+          objetivo: 'Praticar a essência do Front-End durante 7 dias seguidos desenvolvendo uma página a partir de um layout do figma.',
+          ferramentas: 'HTML e CSS',
           img: require("../assets/7daysofcode.png"),
           site: "https://7days-of-code-nine.vercel.app/"
         },
         {
           id: 3,
           title: "Pedra, Papel, Tesoura | FrontEnd Mentor",
+          objetivo: 'Criação de um jogo Jokenpô em javascript com turno de jogador e computador e um contador de pontuação. ',
+          desafio:'No primeiro momento do desenvolvimento meu maior desafio foi a elaboração do algoritmo para a escolha do Computador',
+          ferramentas: 'HTML, SCSS e Javascript para o desenvolvimento da logica do jogo ',
           img: require("../assets/pedrapapeltesoura.png"),
           site: "https://pedra-papel-tesoura-xi-one.vercel.app/"
         },
         {
           id: 4,
           title: "IgniteLab | Rocketseat",
+          objetivo: 'Criação de uma plataforma de curso completa , no qual tenha facilidade para o cadastro de novas aulas por um ferramenta CMS (Content Management System)',
+          ferramentas: 'React, TailwindCSS, GraphQL, Apollo Graph e Hygraph para CMS  ',
           img: require("../assets/ignitelab.png"),
           site: "https://ignitelabreact.vercel.app/"
         },
         {
           id: 5,
           title: "Advinha Número",
+          objetivo: 'Uma simples aplicação de adivinhação de um numero de 1 a 100 em 10 tentativas',
+          ferramentas: 'HTML, CSS e Javascript  ',
           img: require("../assets/advinhaNumero.png"),
           site: "https://advinhanumero.vercel.app/"
         },
