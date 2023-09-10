901adedde7206d24543843bbe7ea86a272e40b95
luquinhazm <lucsmends09@gmail.com>
2023-09-10 16:52:46 -0300

    post page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -8,7 +8,7 @@ export default function PostPage(){
                 <h1 className="text-2xl font-semibold">Olhem Meus Posts</h1>
                 <p className="my-4">Aqui você vai encontrar algumas tentativas minha de explicar sobre o que sei sobre desenvolvimento. tentarei ao maximo de ajudar a tirar suas duvidas</p>
             </div>
-            <div className="flex flex-col gap-6 w-full items-center">
+            <div className="flex flex-col gap-6 w-11/12 items-center">
                 <ListPost></ListPost>
             </div>
         </CMain>
