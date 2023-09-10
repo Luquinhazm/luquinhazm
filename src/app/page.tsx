3cb5ac993f97d0d14160055a4901aaedb8b5554b
luquinhazm <lucsmends09@gmail.com>
2023-09-10 15:55:52 -0300

    responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,21 @@ export default function Home() {
   return (
     <>
       <CMain>
-        <div className="flex justify-between flex-wrap items-center w-[90%] mt-[3vh]">
-          <div className="w-11/12 max-w-[580px]">
+        <div className="flex justify-between flex-wrap items-center w-[90%] min-h-[80vh] max-[680px]:justify-center ">
+        <ProfileImage />
+          <div className="w-[55%] max-w-[580px] max-[680px]:w-11/12">
             <h1 className="text-2xl py-3 font-bold">Olá, sejam bem-vindos &#x1F44B;</h1>
             <p>Eu me chamo Lucas, estudante de <strong>Analise e Desenvolvimento de Sistema</strong> e entusiasta do <strong>Desenvolvimento de Software</strong>. Aqui você poderá encontrar meus textos e projetos</p>
             <IconSocial />
           </div>
-          <ProfileImage />
+        
         </div>
       </CMain>
-      <section className="my-2 mx-auto flex justify-center gap-9 w-[90%]">
-        <div className="w-3/6">
+      <section className="my-2 mx-auto flex flex-wrap justify-center  w-full">
+        <div className="w-[48%] max-[1000px]:w-11/12">
           <ListPost/>
         </div>
-        <div className="w-3/6 flex flex-col items-center justify-center">
+        <div className="w-[48%] max-[1000px]:w-11/12 flex flex-col items-center justify-center">
            <ListProject/>
         </div>
       </section>
--- src/components/ProfileImage/index.tsx
+++ src/components/ProfileImage/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ProfileImage() {
     return (
         <img
-            className="max-h-80 max-w-xs rounded"
+            className="w-[30%] min-h-[270px] min-w-[270px] rounded "
             src="https://avatars.githubusercontent.com/u/90008972?v=4"
             alt=""
         />
