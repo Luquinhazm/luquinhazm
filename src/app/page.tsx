78cb97835714361adca12075b264aa7ae95d33e8
luquinhazm <lucsmends09@gmail.com>
2023-09-12 15:07:28 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <ProfileImage size="w-[30%]"/>
           <div className="w-[55%] max-w-[580px] max-[680px]:w-11/12">
             <h1 className="text-2xl py-3 font-bold">Olá, sejam bem-vindos &#x1F44B;</h1>
-            <p>Eu me chamo Lucas, estudante de <strong>Analise e Desenvolvimento de Sistema</strong> e entusiasta do <strong>Desenvolvimento de Software</strong>. Aqui você poderá encontrar meus textos e projetos</p>
+            <p>Eu me chamo Lucas, estudante de <strong>Analise e Desenvolvimento de Sistema</strong> e entusiasta do <strong>Desenvolvimento de Software</strong>. Aqui você poderá encontrar meus textos e projetos.</p>
             <IconSocial />
           </div>
         
